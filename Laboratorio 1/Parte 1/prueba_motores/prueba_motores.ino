082ef0bef75c7d538965fad0a14a194bbb389deb
Vicente Morales <viice@live.cl>
2025-05-17 23:38:33 -0400

    Actualizaciones
    Motores con velocidad máxima + actualización esquema al 17/05

--- Laboratorio 1/Parte 1/prueba_motores/prueba_motores.ino
+++ Laboratorio 1/Parte 1/prueba_motores/prueba_motores.ino
@@ -6,12 +6,19 @@
 #define IN3 5
 #define IN4 4
 
+#define ENA 9 
+#define ENB 10
+
 void setup() {
   // Definir pines de salida
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+    // Pines de velocidad
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -41,34 +48,49 @@ void loop() {
 void moverAdelante() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
+  analogWrite(ENA, 255);
+
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+  analogWrite(ENB, 255);
 }
 
 void moverAtras() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 255);
+
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 255);
 }
 
 void girarIzquierda() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 255);
+
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+  analogWrite(ENB, 255);
 }
 
 void girarDerecha() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
+  analogWrite(ENA, 255);
+
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 255);
 }
 
 void detener() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
 }
